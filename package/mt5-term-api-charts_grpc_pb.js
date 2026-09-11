f7d1d85eca1982c0a841dff448ee62ecd3f17534
MetaRPC CI <ci@metarpc.pro>
2026-09-11 16:18:32 -0400

    Update NodeMT5 package: feat: isolated EA execution, resource ratio monitoring, auto-termination, and billing integration

--- package/mt5-term-api-charts_grpc_pb.js
+++ package/mt5-term-api-charts_grpc_pb.js
@@ -4,7 +4,52 @@
 var grpc = require('@grpc/grpc-js');
 var mt5$term$api$charts_pb = require('./mt5-term-api-charts_pb.js');
 var google_api_annotations_pb = require('./google/api/annotations_pb.js');
+var google_protobuf_timestamp_pb = require('google-protobuf/google/protobuf/timestamp_pb.js');
 var mrpc$mt5$error_pb = require('./mrpc-mt5-error_pb.js');
+
+function serialize_mt5_term_api_AttachEaReply(arg) {
+  if (!(arg instanceof mt5$term$api$charts_pb.AttachEaReply)) {
+    throw new Error('Expected argument of type mt5_term_api.AttachEaReply');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_mt5_term_api_AttachEaReply(buffer_arg) {
+  return mt5$term$api$charts_pb.AttachEaReply.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_mt5_term_api_AttachEaRequest(arg) {
+  if (!(arg instanceof mt5$term$api$charts_pb.AttachEaRequest)) {
+    throw new Error('Expected argument of type mt5_term_api.AttachEaRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_mt5_term_api_AttachEaRequest(buffer_arg) {
+  return mt5$term$api$charts_pb.AttachEaRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_mt5_term_api_GetEaLogsReply(arg) {
+  if (!(arg instanceof mt5$term$api$charts_pb.GetEaLogsReply)) {
+    throw new Error('Expected argument of type mt5_term_api.GetEaLogsReply');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_mt5_term_api_GetEaLogsReply(buffer_arg) {
+  return mt5$term$api$charts_pb.GetEaLogsReply.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_mt5_term_api_GetEaLogsRequest(arg) {
+  if (!(arg instanceof mt5$term$api$charts_pb.GetEaLogsRequest)) {
+    throw new Error('Expected argument of type mt5_term_api.GetEaLogsRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_mt5_term_api_GetEaLogsRequest(buffer_arg) {
+  return mt5$term$api$charts_pb.GetEaLogsRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
 
 function serialize_mt5_term_api_GetEaParamsReply(arg) {
   if (!(arg instanceof mt5$term$api$charts_pb.GetEaParamsReply)) {
@@ -28,6 +73,28 @@ function deserialize_mt5_term_api_GetEaParamsRequest(buffer_arg) {
   return mt5$term$api$charts_pb.GetEaParamsRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
+function serialize_mt5_term_api_GetRunningEasReply(arg) {
+  if (!(arg instanceof mt5$term$api$charts_pb.GetRunningEasReply)) {
+    throw new Error('Expected argument of type mt5_term_api.GetRunningEasReply');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_mt5_term_api_GetRunningEasReply(buffer_arg) {
+  return mt5$term$api$charts_pb.GetRunningEasReply.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_mt5_term_api_GetRunningEasRequest(arg) {
+  if (!(arg instanceof mt5$term$api$charts_pb.GetRunningEasRequest)) {
+    throw new Error('Expected argument of type mt5_term_api.GetRunningEasRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_mt5_term_api_GetRunningEasRequest(buffer_arg) {
+  return mt5$term$api$charts_pb.GetRunningEasRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
 function serialize_mt5_term_api_OpenTerminalChartWithEaReply(arg) {
   if (!(arg instanceof mt5$term$api$charts_pb.OpenTerminalChartWithEaReply)) {
     throw new Error('Expected argument of type mt5_term_api.OpenTerminalChartWithEaReply');
@@ -48,6 +115,28 @@ function serialize_mt5_term_api_OpenTerminalChartWithEaRequest(arg) {
 
 function deserialize_mt5_term_api_OpenTerminalChartWithEaRequest(buffer_arg) {
   return mt5$term$api$charts_pb.OpenTerminalChartWithEaRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_mt5_term_api_StopEaReply(arg) {
+  if (!(arg instanceof mt5$term$api$charts_pb.StopEaReply)) {
+    throw new Error('Expected argument of type mt5_term_api.StopEaReply');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_mt5_term_api_StopEaReply(buffer_arg) {
+  return mt5$term$api$charts_pb.StopEaReply.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_mt5_term_api_StopEaRequest(arg) {
+  if (!(arg instanceof mt5$term$api$charts_pb.StopEaRequest)) {
+    throw new Error('Expected argument of type mt5_term_api.StopEaRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_mt5_term_api_StopEaRequest(buffer_arg) {
+  return mt5$term$api$charts_pb.StopEaRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
 
@@ -73,6 +162,50 @@ var ChartsService = exports.ChartsService = {
     requestDeserialize: deserialize_mt5_term_api_GetEaParamsRequest,
     responseSerialize: serialize_mt5_term_api_GetEaParamsReply,
     responseDeserialize: deserialize_mt5_term_api_GetEaParamsReply,
+  },
+  attachEa: {
+    path: '/mt5_term_api.Charts/AttachEa',
+    requestStream: false,
+    responseStream: false,
+    requestType: mt5$term$api$charts_pb.AttachEaRequest,
+    responseType: mt5$term$api$charts_pb.AttachEaReply,
+    requestSerialize: serialize_mt5_term_api_AttachEaRequest,
+    requestDeserialize: deserialize_mt5_term_api_AttachEaRequest,
+    responseSerialize: serialize_mt5_term_api_AttachEaReply,
+    responseDeserialize: deserialize_mt5_term_api_AttachEaReply,
+  },
+  getRunningEas: {
+    path: '/mt5_term_api.Charts/GetRunningEas',
+    requestStream: false,
+    responseStream: false,
+    requestType: mt5$term$api$charts_pb.GetRunningEasRequest,
+    responseType: mt5$term$api$charts_pb.GetRunningEasReply,
+    requestSerialize: serialize_mt5_term_api_GetRunningEasRequest,
+    requestDeserialize: deserialize_mt5_term_api_GetRunningEasRequest,
+    responseSerialize: serialize_mt5_term_api_GetRunningEasReply,
+    responseDeserialize: deserialize_mt5_term_api_GetRunningEasReply,
+  },
+  getEaLogs: {
+    path: '/mt5_term_api.Charts/GetEaLogs',
+    requestStream: false,
+    responseStream: false,
+    requestType: mt5$term$api$charts_pb.GetEaLogsRequest,
+    responseType: mt5$term$api$charts_pb.GetEaLogsReply,
+    requestSerialize: serialize_mt5_term_api_GetEaLogsRequest,
+    requestDeserialize: deserialize_mt5_term_api_GetEaLogsRequest,
+    responseSerialize: serialize_mt5_term_api_GetEaLogsReply,
+    responseDeserialize: deserialize_mt5_term_api_GetEaLogsReply,
+  },
+  stopEa: {
+    path: '/mt5_term_api.Charts/StopEa',
+    requestStream: false,
+    responseStream: false,
+    requestType: mt5$term$api$charts_pb.StopEaRequest,
+    responseType: mt5$term$api$charts_pb.StopEaReply,
+    requestSerialize: serialize_mt5_term_api_StopEaRequest,
+    requestDeserialize: deserialize_mt5_term_api_StopEaRequest,
+    responseSerialize: serialize_mt5_term_api_StopEaReply,
+    responseDeserialize: deserialize_mt5_term_api_StopEaReply,
   },
 };
 
--- package/mt5-term-api-connection_pb.js
+++ package/mt5-term-api-connection_pb.js
@@ -3118,7 +3118,6 @@ proto.mt5_term_api.ConnectExRequest.toObject = function(includeInstance, msg) {
     user: jspb.Message.getFieldWithDefault(msg, 1, 0),
     password: jspb.Message.getFieldWithDefault(msg, 2, ""),
     mtClusterName: jspb.Message.getFieldWithDefault(msg, 3, ""),
-    baseChartSymbol: jspb.Message.getFieldWithDefault(msg, 4, ""),
     expertsToAddList: jspb.Message.toObjectList(msg.getExpertsToAddList(),
     proto.mt5_term_api.ExpertAdviser.toObject, includeInstance),
     timeoutSeconds: jspb.Message.getFieldWithDefault(msg, 6, 0),
@@ -3171,10 +3170,6 @@ proto.mt5_term_api.ConnectExRequest.deserializeBinaryFromReader = function(msg, 
     case 3:
       var value = /** @type {string} */ (reader.readString());
       msg.setMtClusterName(value);
-      break;
-    case 4:
-      var value = /** @type {string} */ (reader.readString());
-      msg.setBaseChartSymbol(value);
       break;
     case 5:
       var value = new proto.mt5_term_api.ExpertAdviser;
@@ -3240,13 +3235,6 @@ proto.mt5_term_api.ConnectExRequest.serializeBinaryToWriter = function(message, 
   if (f.length > 0) {
     writer.writeString(
       3,
-      f
-    );
-  }
-  f = /** @type {string} */ (jspb.Message.getField(message, 4));
-  if (f != null) {
-    writer.writeString(
-      4,
       f
     );
   }
@@ -3333,42 +3321,6 @@ proto.mt5_term_api.ConnectExRequest.prototype.getMtClusterName = function() {
  */
 proto.mt5_term_api.ConnectExRequest.prototype.setMtClusterName = function(value) {
   return jspb.Message.setProto3StringField(this, 3, value);
-};
-
-
-/**
- * optional string base_chart_symbol = 4;
- * @return {string}
- */
-proto.mt5_term_api.ConnectExRequest.prototype.getBaseChartSymbol = function() {
-  return /** @type {string} */ (jspb.Message.getFieldWithDefault(this, 4, ""));
-};
-
-
-/**
- * @param {string} value
- * @return {!proto.mt5_term_api.ConnectExRequest} returns this
- */
-proto.mt5_term_api.ConnectExRequest.prototype.setBaseChartSymbol = function(value) {
-  return jspb.Message.setField(this, 4, value);
-};
-
-
-/**
- * Clears the field making it undefined.
- * @return {!proto.mt5_term_api.ConnectExRequest} returns this
- */
-proto.mt5_term_api.ConnectExRequest.prototype.clearBaseChartSymbol = function() {
-  return jspb.Message.setField(this, 4, undefined);
-};
-
-
-/**
- * Returns whether this field is set.
- * @return {boolean}
- */
-proto.mt5_term_api.ConnectExRequest.prototype.hasBaseChartSymbol = function() {
-  return jspb.Message.getField(this, 4) != null;
 };
 
 
@@ -3550,7 +3502,6 @@ proto.mt5_term_api.ConnectByTokenRequest.prototype.toObject = function(opt_inclu
  */
 proto.mt5_term_api.ConnectByTokenRequest.toObject = function(includeInstance, msg) {
   var f, obj = {
-    baseChartSymbol: jspb.Message.getFieldWithDefault(msg, 1, ""),
     timeoutSeconds: jspb.Message.getFieldWithDefault(msg, 2, 0),
     expiration: jspb.Message.getFieldWithDefault(msg, 3, 0)
   };
@@ -3589,10 +3540,6 @@ proto.mt5_term_api.ConnectByTokenRequest.deserializeBinaryFromReader = function(
     }
     var field = reader.getFieldNumber();
     switch (field) {
-    case 1:
-      var value = /** @type {string} */ (reader.readString());
-      msg.setBaseChartSymbol(value);
-      break;
     case 2:
       var value = /** @type {number} */ (reader.readUint32());
       msg.setTimeoutSeconds(value);
@@ -3630,13 +3577,6 @@ proto.mt5_term_api.ConnectByTokenRequest.prototype.serializeBinary = function() 
  */
 proto.mt5_term_api.ConnectByTokenRequest.serializeBinaryToWriter = function(message, writer) {
   var f = undefined;
-  f = /** @type {string} */ (jspb.Message.getField(message, 1));
-  if (f != null) {
-    writer.writeString(
-      1,
-      f
-    );
-  }
   f = /** @type {number} */ (jspb.Message.getField(message, 2));
   if (f != null) {
     writer.writeUint32(
@@ -3651,42 +3591,6 @@ proto.mt5_term_api.ConnectByTokenRequest.serializeBinaryToWriter = function(mess
       f
     );
   }
-};
-
-
-/**
- * optional string base_chart_symbol = 1;
- * @return {string}
- */
-proto.mt5_term_api.ConnectByTokenRequest.prototype.getBaseChartSymbol = function() {
-  return /** @type {string} */ (jspb.Message.getFieldWithDefault(this, 1, ""));
-};
-
-
-/**
- * @param {string} value
- * @return {!proto.mt5_term_api.ConnectByTokenRequest} returns this
- */
-proto.mt5_term_api.ConnectByTokenRequest.prototype.setBaseChartSymbol = function(value) {
-  return jspb.Message.setField(this, 1, value);
-};
-
-
-/**
- * Clears the field making it undefined.
- * @return {!proto.mt5_term_api.ConnectByTokenRequest} returns this
- */
-proto.mt5_term_api.ConnectByTokenRequest.prototype.clearBaseChartSymbol = function() {
-  return jspb.Message.setField(this, 1, undefined);
-};
-
-
-/**
- * Returns whether this field is set.
- * @return {boolean}
- */
-proto.mt5_term_api.ConnectByTokenRequest.prototype.hasBaseChartSymbol = function() {
-  return jspb.Message.getField(this, 1) != null;
 };
 
 
